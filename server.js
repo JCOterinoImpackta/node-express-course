7b7cc91cfad61015be52052a34fdd4acef5df1a3
Juan Carlos <juancarlos.oterino@impackta.com>
2021-10-27 17:05:30 +0200

    añadimos login POST route

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 
 //con esto creamos una instancia del constructor express con nombre APP
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData=[
     {name:'Mark'},
@@ -28,10 +31,30 @@ const mockUserData=[
 })
 
 
+app.post('/login',function(req,res){
+     const username=req.body.username;
+     const password=req.body.password;
+
+     const mockUsername="billyTheKid";
+     const mockPassword="superSecret";
+
+     if (username===mockUsername && password===mockPassword){
+          res.json({
+                success: true,
+                message: 'password and username match!',
+                token: 'encrypted token goes here'
+          })
+     } else {
+          res.json({
+                success: false,
+                message: 'password and username do not match'
+          })
+     }
+})
+
+
 //ya podemos acceder a metodos usados para hacer un servidor incluyendo su nombre despues de app.
 
 //The app.listen method will start up the server locally on the port you give as its first argument 
-app.listen(8000,function(){
-console.log("server is running");
-})
+app.listen(8000,function(){console.log("server is running");})
 
