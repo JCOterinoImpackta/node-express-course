5c8a3ccb7f722bc48074ea4ecb646acc20c2b179
Juan Carlos <juancarlos.oterino@impackta.com>
2021-10-27 08:31:49 +0200

    hago algunas mierdas

---
+++ server.js
@@ -0,0 +1,16 @@
+//acceso a la libreria de expres, accediendo a la carpeta node_modules del proyecto que se ha creado cuando se ha instalado express con 
+// npm install express --save
+const express = require('express');
+
+//con esto creamos una instancia del constructor express con nombre APP
+const app = express();
+
+
+//   ../..
+
+//ya podemos acceder a metodos usados para hacer un servidor incluyendo su nombre despues de app.
+
+//The app.listen method will start up the server locally on the port you give as its first argument 
+app.listen(8000,function){
+console.log("server is running");
+}
